039a3b4ac01bb7a77b7974fdbe3d6f62cf8b70f9
Antra Verma <antraverma99@gmail.com>
2023-12-27 16:08:28 +0530

    reduced padding

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,7 +3,7 @@ import Card from "./components/card";
 
 export default function PortfolioPage() {
   return (
-    <div className="h-auto w-screen flex flex-wrap items-start p-8 justify-center bg-vanilla-cream text-dark-fern text-6xl font-bold gap-5">
+    <div className="h-auto w-screen flex flex-wrap items-start py-4 justify-center bg-vanilla-cream text-dark-fern text-6xl font-bold gap-5">
       {projects.map((project, index) => {
         return <Card key={index} {...project} />;
       })}
