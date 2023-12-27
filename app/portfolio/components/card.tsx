d83ade5a8cf7bcc551918879c78cce9105041551
Antra Verma <antraverma99@gmail.com>
2023-12-27 16:00:57 +0530

    added new project

--- app/portfolio/components/card.tsx
+++ app/portfolio/components/card.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { MouseEvent, useState } from "react";
 
 interface CardProps {
   name: string;
@@ -17,19 +19,40 @@ const Card: React.FC<CardProps> = ({
   liveUrl,
   previewSrc,
 }) => {
+  const [hovered, setHovered] = useState<boolean>(false);
+
+  const _onMouseEnter = (e: MouseEvent<HTMLImageElement, MouseEvent>) => {
+    setHovered(true);
+  };
+
+  const _onMouseLeave = (e: MouseEvent<HTMLImageElement, MouseEvent>) => {
+    setHovered(false);
+  };
+
   return (
-    <div className="bg-white w-11/12 h-auto lg:w-1/3 md:w-1/3 py-8 px-6 rounded-lg shadow-md max-sm:mx-auto">
+    <div className="bg-white w-11/12 h-96 lg:w-1/3 md:w-1/3 py-8 px-6 rounded-lg shadow-md max-sm:mx-auto">
       <h1 className="text-lg">{name}</h1>
-      <Image
-        className="mx-auto my-4 h-auto w-screen"
-        src={previewSrc}
-        width={500}
-        height={250}
-        alt="project-preview"
-        quality={80}
-      />
-      <p className="text-sm">{description}</p>
-      <div className="flex items-center justify-start text-sm mt-6">
+      <div
+        className="relative w-auto h-auto"
+        onMouseEnter={_onMouseEnter}
+        onMouseLeave={_onMouseLeave}
+      >
+        {hovered ? (
+          <div className="mx-auto my-4 h-48 w-auto rounded-lg cursor-pointer bg-moss-green text-sm text-vanilla-cream flex items-center justify-center p-6 bg-opacity-90 scale-75 transition duration-500 transform hover:scale-100">
+            {description}
+          </div>
+        ) : (
+          <Image
+            className="mx-auto my-4 h-48 w-screen rounded-lg cursor-pointer"
+            src={previewSrc}
+            width={500}
+            height={250}
+            alt="project-preview"
+            quality={80}
+          />
+        )}
+      </div>
+      <div className="flex items-center justify-center text-sm mt-6">
         <Link
           href={liveUrl}
           target="_blank"
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,7 +3,7 @@ import Card from "./components/card";
 
 export default function PortfolioPage() {
   return (
-    <div className="h-screen w-screen flex flex-wrap items-start pt-8 justify-center bg-vanilla-cream text-dark-fern text-6xl font-bold">
+    <div className="h-auto w-screen flex flex-wrap items-start p-8 justify-center bg-vanilla-cream text-dark-fern text-6xl font-bold gap-5">
       {projects.map((project, index) => {
         return <Card key={index} {...project} />;
       })}
